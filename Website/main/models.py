59495ee517f7d51ba11a93b396000ee7372b2dfe
ambujalpha <jainambuj8@gmail.com>
2020-03-16 13:48:16 +0530

    social_networks

--- Website/main/models.py
+++ Website/main/models.py
@@ -4,8 +4,18 @@ from django.db import models
 
 class Main(models.Model):
 
-    name = models.TextField()
+    name = models.CharField(max_length=30)
     about = models.TextField()
+    fb = models.CharField(default="-", max_length=30)
+    tw = models.CharField(default="-", max_length=30)
+    yt = models.CharField(default="-", max_length=30)
+    tell = models.CharField(default="-", max_length=30)
+    link = models.CharField(default="-", max_length=30)
+
+    set_name = models.CharField(default="-", max_length=30)
+
+    def __str__(self):
+        return self.set_name + " | " + str(self.pk)
 
 
 
