55ce53bd739bd5d3b021f68a809b7e0fb204f03f
ambujalpha <jainambuj8@gmail.com>
2020-03-20 11:11:46 +0530

    pic_uploading

--- Website/main/urls.py
+++ Website/main/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -7,3 +9,8 @@ urlpatterns = [
     url(r'^about/$', views.about, name='about'),
     url(r'^panel/$', views.panel, name='panel'),
 ]
+
+if settings.DEBUG:
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
