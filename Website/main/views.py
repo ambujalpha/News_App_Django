4168aedbf383891b4097fca21d77aaa821e4030e
ambujalpha <jainambuj8@gmail.com>
2020-03-22 17:23:58 +0530

    menu_dynamic

--- Website/main/views.py
+++ Website/main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Main
 from news.models import News
 from cat.models import Cat
+from subcat.models import SubCat
 
 
 def home(request):
@@ -9,8 +10,10 @@ def home(request):
     site = Main.objects.get(pk=2)
     news = News.objects.all().order_by('-pk')
     cat = Cat.objects.all()
+    subcat = SubCat.objects.all()
+    lastnews = News.objects.all().order_by('-pk')[:3]
 
-    return render(request, 'front/home.html', {'site': site, 'news': news, 'cat': cat})
+    return render(request, 'front/home.html', {'site': site, 'news': news, 'cat': cat, 'subcat': subcat, 'lastnews': lastnews})
 
 
 def about(request):
