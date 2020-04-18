17f22454fcdac5df71700ba30c43cd46e3fe059f
ambujalpha <jainambuj8@gmail.com>
2020-04-18 11:44:34 +0530

    csv_import_export

--- Website/cat/urls.py
+++ Website/cat/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
 
     url(r'^panel/category/list/$', views.cat_list, name='cat_list'),
     url(r'^panel/category/add/$', views.cat_add, name='cat_add'),
+    url(r'^export/cat/csv/$', views.export_cat_csv, name='export_cat_csv'),
+    url(r'^import/cat/csv/$', views.import_cat_csv, name='import_cat_csv'),
 
 ]
--- Website/cat/views.py
+++ Website/cat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Cat
-
+import csv
+from django.http import HttpResponse
 
 def cat_list(request):
 
@@ -35,3 +36,49 @@ def cat_add(request):
         return redirect('cat_list')
 
     return render(request, 'back/cat_add.html')
+
+
+def export_cat_csv(request):
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="cat.csv" '
+
+    writer = csv.writer(response)
+    writer.writerow(['Title','Counter'])
+
+    for i in Cat.objects.all():
+        writer.writerow([i.name, i.count])
+
+    return response
+
+def import_cat_csv(request):
+
+    if request.method == 'POST':
+
+        csv_file = request.FILES['csv_file']
+
+        if not csv_file.name.endswith('.csv'):
+            error = "Please nput csv file"
+            return render(request, 'back/error.html', {'error': error})
+
+        if csv_file.multiple_chunks():
+            error = "File too large"
+            return render(request, 'back/error.html', {'error': error})
+
+        file_data = csv_file.read().decode("utf-8")
+
+        lines = file_data.split("\n")
+
+        for line in lines:
+
+            fields = line.split(",")
+
+            try:
+                if len(Cat.objects.filter(name=fields[0])) == 0 and fields[0] != "Title" and fields[0] != "":
+                    b = Cat(name=fields[0])
+                    b.save()
+
+            except:
+                print("finish")
+
+    return redirect('cat_list')
