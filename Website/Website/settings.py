76d9130a593794524764ae8ac2cbad5ec6442801
ambujalpha <jainambuj8@gmail.com>
2020-04-23 17:24:14 +0530

    Hosting_requirememnts_pakcages_404_error

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6jtt=0i=l6h&0d&*t5+0to0n-g=m-g9%s@m-$4oyvjsdp5nq=g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
